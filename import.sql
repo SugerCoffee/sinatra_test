d1cc95036289eb163ef8b48ee6d8dfd01557f37d
SugerCoffee <soumaforubuntu@gmail.com>
2016-08-24 22:58:33 +0900

    いろいろ書き換えたのでコミット
    いろいろ書き換えたのでコミット

--- import.sql
+++ import.sql
@@ -2,3 +2,12 @@ create table comments(
   id integer primary key,
   body text
 );
+
+create table requests(
+  id integer primary key,
+  name text,
+  reqUrl text,
+  reqType text,
+  reqHead text,
+  reqBody text
+);
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
-require './services/Api_connect_logic.rb'
+require './services/Api_connect_logic'
 
 ActiveRecord::Base.establish_connection(
     "adapter" => "sqlite3",
@@ -11,6 +11,10 @@ ActiveRecord::Base.establish_connection(
 class Comment < ActiveRecord::Base
 end
 
+class Request < ActiveRecord::Base
+end
+
+=begin
 get '/' do
     @title = "BBS"
     @allcomments = Comment.order("id desc").all
@@ -21,14 +25,25 @@ post '/new' do
     Comment.create({:body => params[:body]})
     redirect '/'
 end
+=end
 
 get '/connect_api' do
   @title = 'API接続ツール'
+  @allRequests = Request.order("id desc").all
   erb :api_connect
 end
 
 post '/result_api' do
+  if params[:action] == "save" then
+    Request.create({:name => params[:name], :reqUrl => params[:reqUrl], :reqHead => params[:reqHead], :reqBody => params[:reqBody]})
+    redirect '/connect_api'
+  else
   @title = "API接続結果"
   @res_body = Api_connect_logic.new.get_api(params[:reqUrl])
   erb :api_result, :layout => false
+  end
+end
+
+post '/delete_request' do
+  Request.find(params[:id]).destroy
 end
